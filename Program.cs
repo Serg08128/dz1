5a29dbf6770d0e84bba5f2a66efc69aca66bf40a
Serg08128 <146867399+Serg08128@users.noreply.github.com>
2023-11-29 23:35:34 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,49 +1,4 @@
-﻿// Console.Write("Введите элементы(через пробел): ");
-// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-// int count = 0;
- 
-// for (int i = 0; i < arr.Length; i++)
-// {
-//     if (arr[i] > 0)
-//     {
-//         count++;
-//     }
-// }
- 
-// Console.WriteLine($"Кол-во элементов > 0: {count}");
 
-
-
-// using System;
-
-// class Program
-// {
-//     static void Main()
-//     {
-//         Console.WriteLine("Введите значения k1, b1, k2 и b2 для уравнений y = k1 * x + b1 и y = k2 * x + b2:");
-
-//         // Ввод коэффициентов для первой прямой
-//         Console.Write("k1: ");
-//         double k1 = Convert.ToDouble(Console.ReadLine());
-
-//         Console.Write("b1: ");
-//         double b1 = Convert.ToDouble(Console.ReadLine());
-
-//         // Ввод коэффициентов для второй прямой
-//         Console.Write("k2: ");
-//         double k2 = Convert.ToDouble(Console.ReadLine());
-
-//         Console.Write("b2: ");
-//         double b2 = Convert.ToDouble(Console.ReadLine());
-
-//         // Вычисление точки пересечения
-//         double xIntersection = (b2 - b1) / (k1 - k2);
-//         double yIntersection = k1 * xIntersection + b1;
-
-//         // Вывод результатов
-//         Console.WriteLine($"Точка пересечения: ({xIntersection}, {yIntersection})");
-//     }
-// }
 
 // Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 // N = 5 -> "5, 4, 3, 2, 1"
